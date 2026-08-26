0a79b5bd9e58c147cc917422539f35e951c9063b
donnie-groot <donniegroot@gmail.com>
2026-08-26 01:28:06 +0000

    test: add unit tests for textnode.py

---
+++ src/test_textnode.py
@@ -0,0 +1,26 @@
+import unittest
+from textnode import TextNode, TextType
+
+
+class TestTextNode(unittest.TestCase):
+    def test_eq(self):
+        node = TextNode("This is a text node", TextType.BOLD_TEXT)
+        node2 = TextNode("This is a text node", TextType.BOLD_TEXT)
+        self.assertEqual(node, node2)
+    def test_not_eq(self):
+        node = TextNode("this is a test", TextType.PLAIN_TEXT)
+        node2 = TextNode("this is a test", TextType.ITALIC_TEXT)
+        self.assertNotEqual(node,node2)
+    def test_not_eq_dif_txt(self):
+        node = TextNode("this is a test", TextType.BOLD_TEXT)
+        node2 = TextNode(" this is a diffrent test", TextType.BOLD_TEXT )
+        self.assertNotEqual(node, node2)
+    def test_eq_url_none(self):
+        node = TextNode("this is a test", TextType.BOLD_TEXT, None)
+        node2 = TextNode("this is a test", TextType.BOLD_TEXT, None)
+        self.assertEqual(node, node2)
+            
+
+
+if __name__ == "__main__":
+    unittest.main()
