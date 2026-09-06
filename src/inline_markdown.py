f01512a8b822af4296dc9f1d334eaa3a276c4736
donnie-groot <donniegroot@gmail.com>
2026-09-06 00:31:43 +0000

    feat: Add text_to_textnodes function

--- src/inline_markdown.py
+++ src/inline_markdown.py
@@ -6,6 +6,7 @@ import re
 
 ###### local imports ######
 from textnode import TextNode, TextType
+from splitdelimiter import split_nodes_delimiter
 
 
 
@@ -47,12 +48,8 @@ def split_nodes_image(old_nodes: list[TextNode]) -> list[TextNode]:
         if original_text != "":
             new_nodes.append(TextNode(original_text, TextType.TEXT))
     return new_nodes
-                
-                
-                
-            
-    
 
+    
 def split_nodes_link(old_nodes: list[TextNode]) -> list[TextNode]:
     new_nodes = []
 
@@ -80,3 +77,13 @@ def split_nodes_link(old_nodes: list[TextNode]) -> list[TextNode]:
         if original_text != "":
             new_nodes.append(TextNode(original_text, TextType.TEXT))
     return new_nodes
+
+def text_to_textnodes(text):
+    nodes = [TextNode(text, TextType.TEXT)]
+    nodes = split_nodes_delimiter(nodes, "**", TextType.BOLD)
+    nodes = split_nodes_delimiter(nodes, "_", TextType.ITALIC)
+    nodes = split_nodes_delimiter(nodes, "`", TextType.CODE)
+    nodes = split_nodes_image(nodes)
+    nodes = split_nodes_link(nodes)
+    return nodes
+    
