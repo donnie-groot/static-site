2bdf611832c1eaf4c81283bf034299e575a103bd
donnie-groot <donniegroot@gmail.com>
2026-09-15 04:12:01 +0000

    feat: generate html page from markdown using template

---
+++ src/gencontent.py
@@ -0,0 +1,48 @@
+###### standard library imports ######
+import os 
+
+###### 3rd party imports ######
+#import from thing i downloaded 
+
+###### local imports ######
+from markdown_to_blocks import  markdown_to_html_node
+
+
+
+
+def extract_title(markdown):
+    lines = markdown.split("\n")
+
+    for line in lines:
+        if line.startswith("# "):
+            result = line[2:].strip()
+            return result
+        
+    raise Exception("no title found")
+
+
+def generate_page(from_path, template_path, dest_path):
+    print(f"Generating page from {from_path} to {dest_path} useing {template_path}")
+
+    from_file = open(from_path, "r")
+    markdown_content = from_file.read()
+    from_file.close()
+
+    template_file = open(template_path, "r")
+    template_content = template_file.read()
+    template_file.close()
+
+    node = markdown_to_html_node(markdown_content)
+    html = node.to_html()
+
+    title = extract_title(markdown_content)
+
+    template_content = template_content.replace("{{ Title }}", title)
+    template_content = template_content.replace("{{ Content }}", html)
+
+    des_dir = os.path.dirname(dest_path)
+    os.makedirs(des_dir, exist_ok=True)
+
+    dest_file = open(dest_path, "w")
+    dest_file.write(template_content)
+    dest_file.close()
--- src/main.py
+++ src/main.py
@@ -7,12 +7,13 @@
 ###### local imports ######
 from textnode import TextNode
 from copystatic import copy_file_recursive, delete_and_recreate
+from gencontent import generate_page
 
 
 def main():  
     delete_and_recreate("public")
     copy_file_recursive("static", "public")
-
+    generate_page("content/index.md", "template.html", "public/index.html")
 
 if __name__ == "__main__":
     main()
